b2052b591a938ee655da24cd26ecd5a9a1ac5934
bachirdridi <    GitHub>
2026-05-05 17:35:58 +0100

    fix lab01 and add tests

--- lab01-python/test_factorial.py
+++ lab01-python/test_factorial.py
@@ -1,4 +1,6 @@
-def factorial(n):
-    if n == 0:
-        return 1
-    return n * factorial(n - 1) + 1  # BUG هنا
+from factorial import factorial
+
+def test_factorial():
+    assert factorial(0) == 1
+    assert factorial(1) == 1
+    assert factorial(5) == 120
